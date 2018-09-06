b0481ad0edf504e40e10dae8a345764adbf2c970
Vikram Krishnan <vikram.krishnan@Vikrams-MacBook-Pro.local>
2018-09-06 15:13:59 +0400

    Added provisioners. Edited input variable ingestion for script enabler

--- script-enabler.sh
+++ script-enabler.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-filename=$1
+if [[ -z "${FILENAME_TO_ENABLE}" ]]; then
+    filename=$1
+else
+    filename="${FILENAME_TO_ENABLE}"
+fi
+
 
 sudo update-rc.d -f $filename defaults
 sudo update-rc.d -f $filename enable
